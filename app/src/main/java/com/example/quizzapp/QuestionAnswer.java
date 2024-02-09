3319e1941e54159ec8c6fa5297bef1f5f161b1b6
suhani yadav <2022bcasuhani11625@poornima.edu.in>
2024-02-09 19:37:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/quizzapp/QuestionAnswer.java
@@ -0,0 +1,24 @@
+package com.example.quizzapp;
+
+public class QuestionAnswer {
+    public static  String question[]={
+            "What is the capital of France?",
+            "Who is the author of the Harry Potter series?",
+            " What is the chemical symbol for water?",
+            "Which planet is known as the Red Planet?"
+
+    };
+    public static String choices[][]={
+            {"Paris", "London", "Berlin", "Rome"},
+            {"J.K. Rowling", "Stephen King", "George R.R. Martin", "Agatha Christie"},
+            {"H2O", "CO2", "NaCl", "O2"},
+            {"Mars", "Venus", "Jupiter", "Saturn"}
+    };
+
+    public static String correctAnswer[]={
+            "Green",
+            "J.K. Rowling",
+            "H2O",
+            "Mars"
+    };
+}
